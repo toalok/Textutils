7e670427d845750027b2515bd07d1ae0201d2358
alok <pandeyalok0205@gmail.com>
2023-04-19 16:53:29 +0530

    github component

--- src/component/Github.jsx
+++ src/component/Github.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 export default function Github(props) {
   return (
     <div>
-   Contribute to textutils
+  
+    <h4>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fuga quasi delectus odio explicabo rerum! Sequi obcaecati quidem libero repellat porro in rem id, esse expedita fugiat aspernatur. Quia, mollitia fugit!</h4>
+     <a  target="_blank" href="https://github.com/toalok/Textutils">https://github.com/toalok/Textutils</a>
        
       
     </div>
